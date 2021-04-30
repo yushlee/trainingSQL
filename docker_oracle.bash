168f1d1bd70a6f7b3cb183103beafa3f4a3a72c4
yushlee <ktbwn@hotmail.com>
2021-04-30 07:52:49 -0700

    Update docker_oracle.bash

--- docker_oracle.bash
+++ docker_oracle.bash
@@ -1,27 +1,6 @@
-﻿
-★ tnsnames.ora 路徑
-more ./u01/app/oracle/product/11.2.0/xe/network/admin/tnsnames.ora
-more ./u01/app/oracle/product/11.2.0/xe/network/admin/listener.ora
-cd /u01/app/oracle/product/11.2.0/xe/network/admin
 
-C:/home/oracle
-docker run -d -p 1533:1521 -p 8088:8080 wnameless/oracle-xe-11g-r2
-docker run -d -p 1533:1521 -p 8088:8080 -v C:/home/oracle:/u01/app/oracle/product/11.2.0/xe/network/admin wnameless/oracle-xe-11g-r2
-
-
-http://127.0.0.1:8080/apex/f?p=4950:1:1358154753622568
-http://127.0.0.1:8088/apex/f?p=4950:1:1358154753622568
-
-
-狀態 : 失敗 -測試失敗: Listener refused the connection with the following error:
-ORA-12505, TNS:listener does not currently know of SID given in connect descriptor
-
-狀態 : 失敗 -測試失敗: IO 錯誤: Got minus one from a read call
- 
- https://matthung0807.blogspot.com/2020/10/mac-docker-oracle-12c-docker-container.html
-
-
-https://hub.docker.com/_/mariadb
 docker pull mariadb
+
 docker run -p 127.0.0.1:3306:3306 -e MYSQL_ROOT_PASSWORD=root -d mariadb:latest
+
 https://dev.mysql.com/downloads/workbench/
