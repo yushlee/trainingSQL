bec6989caa115c81b2f22caa49dc79cb23a81c83
yushlee <ktbwn@hotmail.com>
2021-07-02 08:56:27 +0800

    Update MAC_M1_Docker_MySQL.sh
    # Mac with Intel chip
    https://desktop.docker.com/mac/stable/amd64/Docker.dmg
    
    # Mac with Apple chip
    https://desktop.docker.com/mac/stable/arm64/Docker.dmg

--- MAC_M1_Docker_MySQL.sh
+++ MAC_M1_Docker_MySQL.sh
@@ -1,6 +1,12 @@
+# Mac with Intel chip
+https://desktop.docker.com/mac/stable/amd64/Docker.dmg
+
+# Mac with Apple chip
+https://desktop.docker.com/mac/stable/arm64/Docker.dmg
 
 docker pull mariadb
 
 docker run -p 127.0.0.1:3306:3306 -e MYSQL_ROOT_PASSWORD=root -d mariadb:latest
 
+# MySQL workbench
 https://dev.mysql.com/downloads/workbench/
