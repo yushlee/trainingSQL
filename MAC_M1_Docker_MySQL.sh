b5d6a1dc9232c4c0989d608788d56204ce432e9b
yushlee <ktbwn@hotmail.com>
2021-07-05 13:48:15 +0800

    Update MAC_M1_Docker_MySQL.sh

--- MAC_M1_Docker_MySQL.sh
+++ MAC_M1_Docker_MySQL.sh
@@ -4,9 +4,11 @@ https://desktop.docker.com/mac/stable/amd64/Docker.dmg
 # Mac with Apple (m1)核心
 https://desktop.docker.com/mac/stable/arm64/Docker.dmg
 
+# 下載 docker MySQL
 docker pull mariadb
 
+# 啟動 docker MySQL Database
 docker run -p 127.0.0.1:3306:3306 -e MYSQL_ROOT_PASSWORD=root -d mariadb:latest
 
-# MySQL workbench
+# MySQL workbench (開發工具)
 https://dev.mysql.com/downloads/workbench/
