f0260e3f2b6fbabf038919643e0f571a04e44749
yushlee <ktbwn@hotmail.com>
2021-07-07 19:51:06 +0800

    開啟終端機	Finder → 工具程式 → 終端機

--- MAC_M1_Docker_MySQL.sh
+++ MAC_M1_Docker_MySQL.sh
@@ -1,14 +1,15 @@
-# Mac with Intel 核心
+# Mac with Intel核心 下載並且安裝
 https://desktop.docker.com/mac/stable/amd64/Docker.dmg
 
-# Mac with Apple (m1)核心
+# Mac with Apple核心 下載並且安裝
 https://desktop.docker.com/mac/stable/arm64/Docker.dmg
 
 # 下載 docker MySQL
+# 開啟終端機	Finder → 工具程式 → 終端機
 docker pull mariadb
 
 # 啟動 docker MySQL Database
 docker run -p 127.0.0.1:3306:3306 -e MYSQL_ROOT_PASSWORD=root -d mariadb:latest
 
-# MySQL workbench (開發工具) 下載並且安裝即可
+# MySQL workbench (開發工具) 下載並且安裝
 https://dev.mysql.com/downloads/workbench/
