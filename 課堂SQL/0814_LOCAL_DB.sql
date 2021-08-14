3cd8dc9cc174caf7eada66e31d3d02cebe4c6f90
yushlee <ktbwn@hotmail.com>
2021-08-14 11:10:01 +0800

    0814_LOCAL_DB.sql

--- 課堂SQL/0814_LOCAL_DB.sql
+++ 課堂SQL/0814_LOCAL_DB.sql
@@ -1,9 +1,18 @@
 
 -- 針對"加總後"的各別商店營業額做"條件篩選"
--- 對函數產生的值來設定條件查尋\
+-- 對函數產生的值來設定條件查尋
 -- GROUP BY → Function → HAVING → ORDER BY
 SELECT STORE_NAME, SUM(SALES)
 FROM STORE_INFORMATION
 GROUP BY STORE_NAME
 HAVING SUM(SALES) >= 3000
 ORDER BY SUM(SALES) DESC;
+
+
+-- 計算和統計「個別商店」的以下三項資料：
+-- 「總合營業額」、「商店資料個數」、「平均營業額」
+-- 搜尋或排除條件如下：
+-- 排除「平均營業額」1000(含)以下的商店資料
+-- 排除「商店資料個數」1(含)個以下的商店資料
+-- 依照「平均營業額」由大至小排序
+
