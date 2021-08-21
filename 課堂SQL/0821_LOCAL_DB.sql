8afe653dcc1ae46c8cf1309ee1e0dfed6fae1793
yushlee <ktbwn@hotmail.com>
2021-08-21 16:16:51 +0800

    0821_LOCAL_DB.sql

--- 課堂SQL/0821_LOCAL_DB.sql
+++ 課堂SQL/0821_LOCAL_DB.sql
@@ -151,6 +151,10 @@ ORDER BY DEP_EMP.AVG_DEP DESC, E.SALARY DESC;
 --   參考完整性(referential integrity)
 --   父檔:顧客資料表，子檔:訂單資料表
 --  一般公司為了效能考量,並不會真的去設置FK欄位限制!
+-- 	5.1:子項列再建資料時找不到相對應的父項編號
+-- 	Cannot add or update a child row: a foreign key constraint fails
+-- 	5.2:再刪除父項資料列之前，必須沒有任何的子項資料
+-- 	Cannot delete or update a parent row: a foreign key constraint fails
 
 -- Oracle
 CREATE TABLE TABLE1 (
@@ -182,14 +186,117 @@ CREATE TABLE STORE_INFORMATION (
  	CONSTRAINT CONSTRAINT_GEOGRAPHY_ID FOREIGN KEY (GEOGRAPHY_ID) REFERENCES GEOGRAPHY (GEOGRAPHY_ID)
 );
 
--- 子項列再建資料時找不到相對應的父項編號
--- Cannot add or update a child row: a foreign key constraint fails
 
--- 再刪除父項資料列之前，必須沒有任何的子項資料
--- Cannot delete or update a parent row: a foreign key constraint fails
+-- 1.One to One 一對一關係
+-- A資料表中的單筆資料記錄同時只能對應到B資料表的一筆記錄
+--  Duplicate entry '110' for key 'state.GOV_SDID'
+-- https://dataedo.com/kb/tools/mysql-workbench/create-database-diagram
+
+-- 政府官員州長
+CREATE TABLE Gov(
+    GID NUMERIC(3) PRIMARY KEY,
+    Name VARCHAR(25),
+    Address VARCHAR(30),
+    TermBegin date,
+    TermEnd date
+);
+
+-- 州
+-- REFERENCES(參照)、CONSTRAINT(限制)、UNIQUE(唯一)
+CREATE TABLE State(
+    SID NUMERIC(3) PRIMARY KEY,
+    StateName VARCHAR(15),
+    Population NUMERIC(10),
+    SGID NUMERIC(4) REFERENCES Gov(GID),
+    CONSTRAINT GOV_SDID UNIQUE (SGID)
+);
+
+INSERT INTO Gov (GID, Name, Address, TermBegin, TERMEND) VALUES (110, 'Bob', '123 Any St', '2009-01-01', '2011-12-31');
+INSERT INTO State (SID, StateName, Population, SGID) VALUES (111, 'Virginia', 2000000, 110);
 
 
+-- 2.One to Many 一對多關係
+-- A資料表中的單筆資料記錄同時可以對應到B資料表的多筆記錄
+-- 例如一間供用商可同時有多個商品，但一個商品只能屬於一間供應商
 
+-- 供應商
+CREATE TABLE Vendor(
+    VendorNUMERIC NUMERIC(4) PRIMARY KEY,
+    Name VARCHAR(20),
+    Address VARCHAR(200),
+    City VARCHAR(15),
+    Street VARCHAR(200),
+    ZipCode VARCHAR(10),
+    PhoneNUMERIC VARCHAR(12),
+    Status VARCHAR(50)
+);
+
+-- 商品清單
+CREATE TABLE Inventory(
+    Item VARCHAR(50) PRIMARY KEY,
+    Description VARCHAR(300),
+    CurrentQuantity NUMERIC(4) NOT NULL,
+ 	VendorNUMERIC NUMERIC(4),
+ 	CONSTRAINT CONSTRAINT_Vendor_ID FOREIGN KEY (VendorNUMERIC) REFERENCES Vendor (VendorNUMERIC)
+);
+
+INSERT INTO Vendor (VENDORNUMERIC, NAME, ADDRESS, CITY, STREET, ZIPCODE, PHONENUMERIC, STATUS) VALUES ('1', 'Apple Inc', '大同區承德路一段1號1樓', '台北市', '承德路', '10351', '02 7743 8068', '營運中');
+INSERT INTO Inventory (ITEM, DESCRIPTION, CURRENTQUANTITY, VENDORNUMERIC) VALUES ('iPhone 7 Plus', 'iPhone 7 Plus 5.5吋手機 32GB(原廠包裝盒+原廠配件)', '10', '1');
+
+
+-- Many to Many 多對多關係
+-- A資料表中的多筆資料記錄同時可以對應到B資料表的多筆記錄
+-- 例如一位學生可以選擇多門課，一門課也可以同時被多位學生選擇
+
+-- 課程科目
+CREATE TABLE Class(
+    ClassID VARCHAR(20) PRIMARY KEY,
+    ClassName VARCHAR(300),
+    Instructor VARCHAR(100)
+);
+
+-- 學生
+CREATE TABLE Student(
+    StudentID VARCHAR(20) PRIMARY KEY,
+    Name VARCHAR(100),
+    Major VARCHAR(100),
+    ClassYear VARCHAR(50)
+);
+
+--
+-- UNIQUE (StudentID, ClassID)
+-- 表示一位學生只能選擇同樣的課程一次不得重覆
+CREATE TABLE ClassStudent_Relation(
+    StudentID VARCHAR(20) NOT NULL,
+    ClassID VARCHAR(20) NOT NULL,
+    FOREIGN KEY (StudentID) REFERENCES Student(StudentID),
+    FOREIGN KEY (ClassID) REFERENCES Class(ClassID),
+    UNIQUE (StudentID, ClassID)
+);
+
+INSERT INTO Class (ClassID, ClassNAME, INSTRUCTOR) VALUES ('1', '國文', '朱媽');
+INSERT INTO Class (ClassID, ClassNAME, INSTRUCTOR) VALUES ('2', '數學', '凡清');
+INSERT INTO Class (ClassID, ClassNAME, INSTRUCTOR) VALUES ('3', '英文', '高國華');
+INSERT INTO Class (ClassID, ClassNAME, INSTRUCTOR) VALUES ('4', '理化', '阿飛');
+INSERT INTO Class (ClassID, ClassNAME, INSTRUCTOR) VALUES ('5', '物理', '簡杰');
+INSERT INTO Class (ClassID, ClassNAME, INSTRUCTOR) VALUES ('6', '歷史', '呂杰');
+INSERT INTO Class (ClassID, ClassNAME, INSTRUCTOR) VALUES ('7', '地理', '王剛');
+
+INSERT INTO Student (StudentID, NAME, MAJOR, ClassYEAR) VALUES ('1', '馬小九', '資訊管理', '大二');
+INSERT INTO Student (StudentID, NAME, MAJOR, ClassYEAR) VALUES ('2', '輸真慘', '資訊工程', '大一');
+INSERT INTO Student (StudentID, NAME, MAJOR, ClassYEAR) VALUES ('3', '菜英蚊', '企業管理', '大三');
+INSERT INTO Student (StudentID, NAME, MAJOR, ClassYEAR) VALUES ('4', '豬利輪', '財務金融', '大二');
+INSERT INTO Student (StudentID, NAME, MAJOR, ClassYEAR) VALUES ('5', '韓國魚', '應用外語', '碩二');
+INSERT INTO Student (StudentID, NAME, MAJOR, ClassYEAR) VALUES ('6', '賣臺銘', '國際貿易', '大一');
+
+INSERT INTO ClassStudent_Relation (StudentID, ClassID) VALUES ('1', '1');
+INSERT INTO ClassStudent_Relation (StudentID, ClassID) VALUES ('1', '3');
+INSERT INTO ClassStudent_Relation (StudentID, ClassID) VALUES ('2', '1');
+INSERT INTO ClassStudent_Relation (StudentID, ClassID) VALUES ('3', '1');
+INSERT INTO ClassStudent_Relation (StudentID, ClassID) VALUES ('3', '2');
+INSERT INTO ClassStudent_Relation (StudentID, ClassID) VALUES ('3', '5');
+INSERT INTO ClassStudent_Relation (StudentID, ClassID) VALUES ('5', '6');
+INSERT INTO ClassStudent_Relation (StudentID, ClassID) VALUES ('6', '6');
 
 
 
