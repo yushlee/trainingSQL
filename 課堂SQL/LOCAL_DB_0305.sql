76b1211c017e71f78b57d82d9ab07b047334e246
yushlee <ktbwn@hotmail.com>
2022-03-05 14:08:20 +0800

    LOCAL_DB_0305.sql

--- 課堂SQL/LOCAL_DB_0305.sql
+++ 課堂SQL/LOCAL_DB_0305.sql
@@ -1,6 +1,8 @@
 
---   SQL UNION 聯集(不包含重覆值)
--- UNION可下多次
+-- SQL UNION 聯集(不包含重覆值)
+-- 1.UNION 可下多次
+-- 2.UNION SQL之間欄位"型態"必須一致
+-- 3.UNION SQL之間欄位資料個數必須一致
 SELECT G.GEOGRAPHY_ID, G.REGION_NAME, S.store_id, S.store_name 
 FROM geography G
 LEFT JOIN store_information S ON G.GEOGRAPHY_ID = S.geography_id
@@ -8,5 +10,12 @@ UNION
 SELECT G.GEOGRAPHY_ID, G.REGION_NAME, S.store_id, S.store_name 
 FROM geography G
 RIGHT JOIN store_information S ON G.GEOGRAPHY_ID = S.geography_id;
+
+
+SELECT STORE_ID, STORE_NAME FROM store_information
+UNION
+SELECT GEOGRAPHY_ID, REGION_NAME FROM geography;
+
+
 
 
