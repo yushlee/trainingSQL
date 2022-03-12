26ddf252f2866e30d5b3baf22767cb6250b02530
yushlee <ktbwn@hotmail.com>
2022-03-12 11:20:25 +0800

    LOCAL_DB_0312.sql

---
+++ 課堂SQL/LOCAL_DB_0312.sql
@@ -0,0 +1,8 @@
+SELECT STORE_NAME,SALES,STORE_ID FROM store_information;
+
+SELECT * FROM store_information;
+
+SELECT DISTINCT STORE_NAME FROM store_information;
+
+-- DISTINCT去重覆是以整列資料為去重覆的依據
+SELECT  DISTINCT STORE_ID,STORE_NAME FROM store_information;
