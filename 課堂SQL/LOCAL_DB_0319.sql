53687f6154323174f9f4842bf83c86efd747af36
yushlee <ktbwn@hotmail.com>
2022-03-19 11:22:11 +0800

    LOCAL_DB_0319.sql

--- 課堂SQL/LOCAL_DB_0319.sql
+++ 課堂SQL/LOCAL_DB_0319.sql
@@ -9,9 +9,38 @@ SELECT S.*, G.*
 FROM store_information S, geography G
 WHERE S.geography_id = G.GEOGRAPHY_ID;
 
--- INNER JOIN...ON
+-- INNER JOIN...ON(內部連接)
 SELECT S.*, G.*
 FROM store_information S 
 INNER JOIN geography G ON S.geography_id = G.GEOGRAPHY_ID
 WHERE S.sales > 2000;
 
+
+SELECT G.*,S.*
+FROM geography G INNER JOIN store_information S ON G.GEOGRAPHY_ID = S.geography_id;
+
+-- INNER 可省略
+SELECT G.*,S.*
+FROM geography G JOIN store_information S ON G.GEOGRAPHY_ID = S.geography_id;
+
+-- OUTER JOIN ON (外部連結)
+-- OUTER可省略
+SELECT G.*,S.*
+FROM geography G LEFT OUTER JOIN store_information S ON G.GEOGRAPHY_ID = S.geography_id;
+
+SELECT G.*,S.*
+FROM geography G 
+LEFT JOIN store_information S ON G.GEOGRAPHY_ID = S.geography_id;
+
+
+SELECT G.*,S.*
+FROM geography G 
+LEFT JOIN store_information S ON G.GEOGRAPHY_ID = S.geography_id;
+
+
+SELECT G.*,S.*
+FROM geography G RIGHT JOIN store_information S ON G.GEOGRAPHY_ID = S.geography_id;
+
+-- MySQL不支援 FULL JOIN
+SELECT G.*,S.*
+FROM geography G FULL JOIN store_information S ON G.GEOGRAPHY_ID = S.geography_id;
