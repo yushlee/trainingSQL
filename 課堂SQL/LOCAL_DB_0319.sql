f8d5ec4a46dc83f3d6426282553d7b9f3aa82b75
yushlee <ktbwn@hotmail.com>
2022-03-19 13:56:28 +0800

    LOCAL_DB_0319.sql

--- 課堂SQL/LOCAL_DB_0319.sql
+++ 課堂SQL/LOCAL_DB_0319.sql
@@ -44,3 +44,65 @@ FROM geography G RIGHT JOIN store_information S ON G.GEOGRAPHY_ID = S.geography_
 -- MySQL不支援 FULL JOIN
 SELECT G.*,S.*
 FROM geography G FULL JOIN store_information S ON G.GEOGRAPHY_ID = S.geography_id;
+
+-- 由不同欄位獲得的資料串連在一起 
+SELECT CONCAT(STORE_ID, '-', STORE_NAME, ' $',SALES) FROM store_information;
+
+
+-- Oracle CONCAT只允許兩個參數
+SELECT CONCAT(STORE_ID,STORE_NAME) FROM store_information;
+-- Oracle || 符號字串相連
+SELECT STORE_ID || '-' || STORE_NAME FROM store_information;
+
+
+-- MS SQL + 
+SELECT G.REGION_NAME + '-' + S.STORE_NAME, S.STORE_DATE 
+FROM GEOGRAPHY G JOIN STORE_INFORMATION S
+ON G.GEOGRAPHY_ID = S.GEOGRAPHY_ID;
+
+
+-- substring 子字串
+-- SUBSTR(str,pos): 由<str>中，選出所有從第<pos>位置
+-- 開始的字元。請注意，這個語法不適用於SQL Server上
+-- SUBSTR(str,pos,len): 由<str>中的第<pos>位置開始，選出
+-- 接下去的<len>個字元。
+SELECT STORE_NAME, SUBSTR(STORE_NAME, 3), SUBSTR(STORE_NAME, 3, 6)
+FROM store_information;
+
+
+SELECT SUBSTR('ABCDEFGHIJK', 3, 2);
+
+
+-- TRIM([[位置] [要移除的字串] FROM ] 字串)
+-- [位置] 的可能值為 LEADING (起頭), TRAILING (結尾), or BOTH (起頭及結尾)
+SELECT STORE_ID, STORE_NAME,
+    TRIM(LEADING 'Bos' FROM STORE_NAME),
+    TRIM(TRAILING 's' FROM STORE_NAME),
+    TRIM(BOTH 'L' FROM STORE_NAME)
+FROM STORE_INFORMATION;
+
+
+-- 查詢各區域的營業額總計
+-- 資料結果依營業額總計由大到小排序
+-- (不論該區域底下是否有所屬商店)
+
+SELECT G.*, S.*
+FROM geography G JOIN store_information S
+ON G.GEOGRAPHY_ID = S.geography_id
+ORDER BY G.GEOGRAPHY_ID;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
