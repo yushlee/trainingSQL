b9b9b555943c63365dedce0df6e39cdc4623a6f6
yushlee <ktbwn@hotmail.com>
2022-03-26 15:10:24 +0800

    LOCAL_DB_0326.sql

---
+++ 課堂SQL/LOCAL_DB_0326.sql
@@ -0,0 +1,17 @@
+SELECT * FROM geography;
+
+SELECT * FROM store_information;
+
+-- One to One
+SELECT * FROM GOV;
+SELECT * FROM STATE;
+
+-- Ont to Many
+SELECT * FROM VENDOR;
+SELECT * FROM INVENTORY;
+
+-- Many to Many
+SELECT * FROM CLASS;
+SELECT * FROM STUDENT;
+SELECT * FROM classstudent_relation;
+
