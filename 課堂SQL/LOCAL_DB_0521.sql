111de04b27c384dcb155ba18e47e51a3a968be45
yushlee <ktbwn@hotmail.com>
2022-05-21 14:38:39 +0800

    LOCAL_DB_0521.sql

--- 課堂SQL/LOCAL_DB_0521.sql
+++ 課堂SQL/LOCAL_DB_0521.sql
@@ -20,14 +20,40 @@ UNION ALL
 SELECT REGION_NAME FROM geography;
 
 
---   SQL INTERSECT 交集(PS:MySQL不支援INTERSECT)
+--   SQL INTERSECT 交集 (PS:MySQL不支援INTERSECT)
 -- 請注意，在 INTERSECT 指令下，不同的值只會被列出一次。 
 -- 1、2、3
 SELECT GEOGRAPHY_ID FROM geography
 INTERSECT
 SELECT GEOGRAPHY_ID FROM store_information;
 
---   SQL MINUS 排除(不包含重覆值) 
+
+-- https://www.yiibai.com/mysql/sql-union-mysql.html
+-- INNER JOIN + DISTINCT = INTERSECT(不包含重覆值)查詢交集結果
+SELECT DISTINCT G.GEOGRAPHY_ID 
+FROM geography G
+JOIN store_information S ON G.GEOGRAPHY_ID  = S.GEOGRAPHY_ID;
+
+
+-- SQL MINUS 排除(不包含重覆值) 
+-- (PS:MySQL不支援 MINUS)
+-- 先找出第一個 SQL 語句所產生的結果，
+-- 然後看這些結果「有沒有在第二個 SQL 語句的結果中」。
+-- 如果「有」的話，那這一筆資料就被「去除」，而不會在最後的結果中出現。
+-- 如果「沒有」的話，那這一筆資料就被「保留」，而就會在最後的結果中出現。
+SELECT GEOGRAPHY_ID FROM geography
+MINUS
+SELECT GEOGRAPHY_ID FROM store_information;
+
+
+-- https://www.yiibai.com/mysql/minus.html
+-- LEFT JOIN + table2.id IS NULL = MINUS
+SELECT G.GEOGRAPHY_ID 
+FROM geography G
+LEFT JOIN store_information S ON G.GEOGRAPHY_ID = S.GEOGRAPHY_ID
+WHERE S.GEOGRAPHY_ID IS NULL;
+
+
 --   SQL SubQuery 子查詢
 --   SQL EXISTS 存在式關聯查詢
 --   SQL CASE WHEN 條件查詢
