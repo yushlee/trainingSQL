89c97ea5239dbf0909b0806bfc5dc3580aca1d54
yushlee <ktbwn@hotmail.com>
2022-09-04 15:46:33 +0800

    LOCAL_DB_0904.sql

--- 課堂SQL/LOCAL_DB_0904.sql
+++ 課堂SQL/LOCAL_DB_0904.sql
@@ -32,7 +32,6 @@ CREATE TABLE STORE_INFORMATION(
 );
 
 -- One to One
-
 -- 政府官員州長
 CREATE TABLE Gov(
     GID NUMERIC(3) PRIMARY KEY,
@@ -62,6 +61,97 @@ SELECT * FROM Gov;
 
 SELECT * FROM STATE;
 
+
+-- One to Many
+-- 供應商
+CREATE TABLE Vendor(
+    VendorNUMERIC NUMERIC(4) PRIMARY KEY,
+    Name VARCHAR(20),
+    Address VARCHAR(200),
+    City VARCHAR(15),
+    Street VARCHAR(200),
+    ZipCode VARCHAR(10),
+    PhoneNUMERIC VARCHAR(12),
+    Status VARCHAR(50)
+);
+
+-- 商品清單
+CREATE TABLE Inventory(
+    Item VARCHAR(50) PRIMARY KEY,
+    Description VARCHAR(300),
+    CurrentQuantity NUMERIC(4) NOT NULL,
+    VendorNUMERIC NUMERIC(4),
+	CONSTRAINT CONSTRAINT_VENDOR_FK FOREIGN KEY (VendorNUMERIC) REFERENCES Vendor(VendorNUMERIC)
+);
+
+INSERT INTO Vendor (VENDORNUMERIC, NAME, ADDRESS, CITY, STREET, ZIPCODE, PHONENUMERIC, STATUS) VALUES ('1', 'Apple Inc', '大同區承德路一段1號1樓', '台北市', '承德路', '10351', '02 7743 8068', '營運中');
+INSERT INTO Inventory (ITEM, DESCRIPTION, CURRENTQUANTITY, VENDORNUMERIC) VALUES ('iPhone 7 Plus', 'iPhone 7 Plus 5.5吋手機 32GB(原廠包裝盒+原廠配件)', '10', '1');
+
+SELECT * FROM Vendor;
+
+SELECT * FROM Inventory;
+
+
+
+-- Many to Many
+-- 課程科目
+
+DROP TABLE ClassStudent_Relation;
+DROP TABLE Class;
+DROP TABLE Student;
+
+CREATE TABLE Class(
+    ClassID VARCHAR(20) PRIMARY KEY,
+    ClassName VARCHAR(300),
+    Instructor VARCHAR(100)
+);
+
+-- 學生
+CREATE TABLE Student(
+    StudentID VARCHAR(20) PRIMARY KEY,
+    Name VARCHAR(100),
+    Major VARCHAR(100),
+    ClassYear VARCHAR(50)
+);
+
+--
+-- UNIQUE (StudentID, ClassID)
+-- 表示一位學生只能選擇同樣的課程一次不得重覆
+CREATE TABLE ClassStudent_Relation(
+    StudentID VARCHAR(20) NOT NULL,
+    ClassID VARCHAR(20) NOT NULL,
+    FOREIGN KEY (StudentID) REFERENCES Student(StudentID),
+    FOREIGN KEY (ClassID) REFERENCES Class(ClassID),
+    UNIQUE (StudentID, ClassID)
+);
+
+INSERT INTO Class (ClassID, ClassNAME, INSTRUCTOR) VALUES ('1', '國文', '朱媽');
+INSERT INTO Class (ClassID, ClassNAME, INSTRUCTOR) VALUES ('2', '數學', '凡清');
+INSERT INTO Class (ClassID, ClassNAME, INSTRUCTOR) VALUES ('3', '英文', '高國華');
+INSERT INTO Class (ClassID, ClassNAME, INSTRUCTOR) VALUES ('4', '理化', '阿飛');
+INSERT INTO Class (ClassID, ClassNAME, INSTRUCTOR) VALUES ('5', '物理', '簡杰');
+INSERT INTO Class (ClassID, ClassNAME, INSTRUCTOR) VALUES ('6', '歷史', '呂杰');
+INSERT INTO Class (ClassID, ClassNAME, INSTRUCTOR) VALUES ('7', '地理', '王剛');
+
+INSERT INTO Student (StudentID, NAME, MAJOR, ClassYEAR) VALUES ('1', '馬小九', '資訊管理', '大二');
+INSERT INTO Student (StudentID, NAME, MAJOR, ClassYEAR) VALUES ('2', '輸真慘', '資訊工程', '大一');
+INSERT INTO Student (StudentID, NAME, MAJOR, ClassYEAR) VALUES ('3', '菜英蚊', '企業管理', '大三');
+INSERT INTO Student (StudentID, NAME, MAJOR, ClassYEAR) VALUES ('4', '豬利輪', '財務金融', '大二');
+INSERT INTO Student (StudentID, NAME, MAJOR, ClassYEAR) VALUES ('5', '韓國魚', '應用外語', '碩二');
+INSERT INTO Student (StudentID, NAME, MAJOR, ClassYEAR) VALUES ('6', '賣臺銘', '國際貿易', '大一');
+
+INSERT INTO ClassStudent_Relation (StudentID, ClassID) VALUES ('1', '1');
+INSERT INTO ClassStudent_Relation (StudentID, ClassID) VALUES ('1', '3');
+INSERT INTO ClassStudent_Relation (StudentID, ClassID) VALUES ('2', '1');
+INSERT INTO ClassStudent_Relation (StudentID, ClassID) VALUES ('3', '1');
+INSERT INTO ClassStudent_Relation (StudentID, ClassID) VALUES ('3', '2');
+INSERT INTO ClassStudent_Relation (StudentID, ClassID) VALUES ('3', '5');
+INSERT INTO ClassStudent_Relation (StudentID, ClassID) VALUES ('5', '6');
+INSERT INTO ClassStudent_Relation (StudentID, ClassID) VALUES ('6', '6');
+
+SELECT * FROM Student;
+SELECT * FROM Class;
+SELECT * FROM ClassStudent_Relation;
 
 
 
