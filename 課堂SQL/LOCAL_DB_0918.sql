fd183db7d81cd49028769618eed3929b519aa150
yushlee <ktbwn@hotmail.com>
2022-09-18 09:54:41 +0800

    LOCAL_DB_0918.sql

---
+++ 課堂SQL/LOCAL_DB_0918.sql
@@ -0,0 +1,24 @@
+--   SQL UNION 聯集(不包含重覆值)
+
+-- 1,2,3
+SELECT GEOGRAPHY_ID FROM geography
+UNION
+-- null,1,2
+SELECT GEOGRAPHY_ID FROM store_information;
+
+-- 可以透過 UNION(聯集) 將 LEFT JOIN + RIGHT JOIN = FULL JOIN
+SELECT G.GEOGRAPHY_ID, G.REGION_NAME, S.STORE_ID, S.STORE_NAME
+FROM geography G 
+LEFT JOIN store_information S ON G.GEOGRAPHY_ID = S.GEOGRAPHY_ID
+UNION
+SELECT G.GEOGRAPHY_ID, G.REGION_NAME, S.STORE_ID, S.STORE_NAME
+FROM geography G 
+RIGHT JOIN store_information S ON G.GEOGRAPHY_ID = S.GEOGRAPHY_ID;
+
+
+--   SQL UNION ALL 聯集(包含重覆值)
+--   SQL INTERSECT 交集
+--   SQL MINUS 排除(不包含重覆值) 
+--   SQL SubQuery 子查詢
+--   SQL EXISTS 存在式關聯查詢
+--   SQL CASE WHEN 條件查詢
