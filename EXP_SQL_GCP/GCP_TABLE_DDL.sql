e4bc45801da1ea15e363109ccd8e0876f00fac36
yushlee <ktbwn@hotmail.com>
2024-03-03 13:24:27 +0800

    GCP SQL

---
+++ EXP_SQL_GCP/GCP_TABLE_DDL.sql
@@ -0,0 +1,18 @@
+/*
+
+GCP BigQuery
+https://console.cloud.google.com/bigquery
+
+*/
+CREATE TABLE momo-search.store.GEOGRAPHY (
+	GEOGRAPHY_ID NUMERIC,
+	REGION_NAME  STRING
+);
+
+CREATE TABLE momo-search.store.STORE_INFORMATION(
+	STORE_ID   NUMERIC,
+	STORE_NAME STRING,
+	SALES      NUMERIC,
+	STORE_DATE TIMESTAMP,
+ 	GEOGRAPHY_ID NUMERIC
+);
